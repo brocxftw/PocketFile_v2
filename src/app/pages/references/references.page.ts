965d5b9aed200ef6bdbd459ea6d852e4528affe9
brocxftw <joebrocxfreeman@gmail.com>
2021-05-27 21:42:56 +0800

    Added reference array to be parsed in reference view.

--- src/app/pages/references/references.page.ts
+++ src/app/pages/references/references.page.ts
@@ -7,6 +7,105 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReferencesPage implements OnInit {
 
+  references = [
+    { 
+      kod: 'M 1 TD',
+      name: 'The Army',
+      url: '',
+    },
+    { 
+      kod: '',
+      name: 'The Enemy (Lanun)',
+      url: '',
+    },
+    { 
+      kod: 'MD 3.0 TD',
+      name: 'Operations',
+      url: '',
+    },
+    { 
+      kod: '',
+      name: 'The Infantry Platoon',
+      url: '',
+    },
+    { 
+      kod: '',
+      name: 'MAFJP',
+      url: '',
+    },
+    { 
+      kod: '',
+      name: 'Panduan Ketua Seksyen',
+      url: '',
+    },
+    { 
+      kod: '',
+      name: 'Meronda dan Mengesan',
+      url: '',
+    },
+    { 
+      kod: '',
+      name: 'Operasi Hutan',
+      url: '',
+    },
+    { 
+      kod: '',
+      name: 'Aide Memoire Operasi Medan Pegawai Muda dan PTT',
+      url: '',
+    },
+    { 
+      kod: '',
+      name: 'Aide Memoire Operasi Medan Pegawai Muda dan PTT',
+      url: '',
+    },
+    { 
+      kod: '',
+      name: 'Aide Memoire Operasi Medan Pegawai Muda dan PTT',
+      url: '',
+    },
+    { 
+      kod: '',
+      name: 'Aide Memoire Operasi Medan Pegawai Muda dan PTT',
+      url: '',
+    },
+    { 
+      kod: '',
+      name: 'Aide Memoire Operasi Medan Pegawai Muda dan PTT',
+      url: '',
+    },
+    { 
+      kod: '',
+      name: 'Aide Memoire Operasi Medan Pegawai Muda dan PTT',
+      url: '',
+    },
+    { 
+      kod: '',
+      name: 'Aide Memoire Operasi Medan Pegawai Muda dan PTT',
+      url: '',
+    },
+    { 
+      kod: '',
+      name: 'Aide Memoire Operasi Medan Pegawai Muda dan PTT',
+      url: '',
+    },
+    { 
+      kod: '',
+      name: 'Aide Memoire Operasi Medan Pegawai Muda dan PTT',
+      url: '',
+    },
+    { 
+      kod: '',
+      name: 'Aide Memoire Operasi Medan Pegawai Muda dan PTT',
+      url: '',
+    },
+    { 
+      kod: '',
+      name: 'Aide Memoire Operasi Medan Pegawai Muda dan PTT',
+      url: '',
+    },
+    
+  ];
+
   constructor() { }
 
   ngOnInit() {
