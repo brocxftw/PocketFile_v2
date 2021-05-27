48d905ebf6ccc1edc98a3ba97af9e0c70994ca88
brocxftw <joebrocxfreeman@gmail.com>
2021-05-27 11:30:58 +0800

    Login and register points to home

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,9 +11,17 @@ export class LoginPage implements OnInit {
 
   constructor(
     public modalCtrl: ModalController,
+    private router: Router,
   ) { }
 
   ngOnInit() {
+  }
+  
+  async login() {
+    await this.modalCtrl.dismiss();
+
+    // needs auth for homepage
+    this.router.navigateByUrl('home');
   }
 
   async dismiss() {
--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,22 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
 })
+
 export class RegisterPage implements OnInit {
 
   constructor(
     public modalCtrl: ModalController,
+    private router: Router,
   ) { }
 
   ngOnInit() {
   }
 
+  async register() {
+    await this.modalCtrl.dismiss();
+
+    // needs auth for homepage
+    this.router.navigateByUrl('home');
+  }
+
   async dismiss() {
     return await this.modalCtrl.dismiss();
   }
-
 }
