d68ef4aa2a5bf7aabe39a0abef0d287d2e3acca3
brocxftw <joebrocxfreeman@gmail.com>
2021-05-27 20:58:15 +0800

    Changed contacts layout.

--- src/app/pages/contacts/contacts.page.ts
+++ src/app/pages/contacts/contacts.page.ts
@@ -10,7 +10,7 @@ export class ContactsPage implements OnInit {
   contacts = [
     { 
       picture: '',
-      name: 'Mej Zulhadi bin Zulkifli (3543125)',
+      name: 'Mej Mohd Zulhadi bin Zulkifli (3008835)',
       phone: '',
     },
     { 
@@ -20,72 +20,77 @@ export class ContactsPage implements OnInit {
     },
     { 
       picture: '',
-      name: 'Mej Zulhadi bin Zulkifli (3543125)',
+      name: 'Kapt Mohd Fitri bin Zamri (3013144)',
       phone: '',
     },
     { 
       picture: '',
-      name: 'Mej Zulhadi bin Zulkifli (3543125)',
+      name: '1122004 PW2 Cassidy anak Lee',
       phone: '',
     },
     { 
       picture: '',
-      name: 'Mej Zulhadi bin Zulkifli (3543125)',
+      name: '112851 PW2 Syahrizal bin Kamarudin',
       phone: '',
     },
     { 
       picture: '',
-      name: 'Mej Zulhadi bin Zulkifli (3543125)',
+      name: '1136504 SSjn Mohd Razali bin Kasian',
       phone: '',
     },
     { 
       picture: '',
-      name: 'Mej Zulhadi bin Zulkifli (3543125)',
+      name: '1145030 SSjn Saiful Rizan bin Alias',
       phone: '',
     },
     { 
       picture: '',
-      name: 'Mej Zulhadi bin Zulkifli (3543125)',
+      name: '1146177 SSjn Franklin Bernard anak Lagi',
       phone: '',
     },
     { 
       picture: '',
-      name: 'Mej Zulhadi bin Zulkifli (3543125)',
+      name: '1136336 SSjn Iskandar bin Ibrahim',
       phone: '',
     },
     { 
       picture: '',
-      name: 'Mej Zulhadi bin Zulkifli (3543125)',
+      name: '1145703 Sjn Mazlan bin Mohamad',
       phone: '',
     },
     { 
       picture: '',
-      name: 'Mej Zulhadi bin Zulkifli (3543125)',
+      name: '1146610 Sjn Mohammad Aidil bin Adan',
       phone: '',
     },
     { 
       picture: '',
-      name: 'Mej Zulhadi bin Zulkifli (3543125)',
+      name: '1148647 Sjn Clayrence anak Ubang@Radi',
       phone: '',
     },
     { 
       picture: '',
-      name: 'Mej Zulhadi bin Zulkifli (3543125)',
+      name: '1159694 Sjn Mohd Firdaus bin Mohd Hashim',
       phone: '',
     },
     { 
       picture: '',
-      name: 'Mej Zulhadi bin Zulkifli (3543125)',
+      name: '1165090 Sjn Mohamad Ismail bin Musa',
       phone: '',
     },
     { 
       picture: '',
-      name: 'Mej Zulhadi bin Zulkifli (3543125)',
+      name: '1137431 Sjn Mohd Khairil Anuar bin Abd Wahab',
       phone: '',
     },
     { 
       picture: '',
-      name: 'Mej Zulhadi bin Zulkifli (3543125)',
+      name: '1137076 Sjn Muhamad Hamidi bin Darani',
+      phone: '',
+    },
+    { 
+      picture: '',
+      name: '1159345 Sjn Herman bin Jamari',
       phone: '',
     },
   ];
